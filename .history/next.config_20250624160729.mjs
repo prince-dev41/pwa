886655abb6c735b8af6e57a049ff06bcd58ae324
prince-dev41 <princeekpinse97+1@gmail.com>
2025-06-27 09:43:49 +0100

    fix: remove history folder

--- .history/next.config_20250624160729.mjs
+++
@@ -1,7 +0,0 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {};
-const withPWA = require('next-pwa')({
-  dest: 'public',
-  disable: process.env.NEXT_PUBLIC_DISABLE_PWA === 'true', // on pourra désactiver la PWA pour desktop
-});
-export default nextConfig;
--- .history/next.config_20250624160749.mjs
+++
@@ -1,9 +0,0 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {};
-const withPWA = require('next-pwa')({
-  dest: 'public',
-  disable: process.env.NEXT_PUBLIC_DISABLE_PWA === 'true', // on pourra désactiver la PWA pour desktop
-});
-module.exports = withPWA({
-  reactStrictMode: true,
-});
